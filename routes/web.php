f43d46d35b360b81604da28a639fb7d29fb91406
Mobin Poursalami <m.poursalami@xapawallet.com>
2025-07-27 17:35:23 +0330

    Fix Cashier integration: use v15, remove incompatible psalm, adjust platform PHP, add stripe webhook route to handleWebhook

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\StripeWebhookController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
 });
+
+Route::post('/stripe/webhook', [StripeWebhookController::class, 'handleWebhook'])->name('stripe.webhook');
